e71e94ce215b340f48e0679cda29c241403fa001
ItsFaizan <faizanjavid71@gmail.com>
2024-05-12 21:44:20 +0500

    Data Preprocessed

--- data_extraction.py
+++ data_extraction.py
@@ -1,68 +1,37 @@
-# Importing necessary libraries
+# import necessary libraries
 from bs4 import BeautifulSoup
 import requests
+import json
 
-# Function to extract links from dawn.com
-def extract_dawn_links():
-    url = "https://www.dawn.com/"
+# function to extract data from a webpage
+def extract_data(url):
+    # send a GET request to the URL
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = [link.get('href') for link in soup.find_all('a')]
-    return links
+    # parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # extract links, titles, and descriptions
+    links = [link['href'] for link in soup.find_all('a')]
+    titles = [title.get_text() for title in soup.find_all('h2')]
+    descriptions = [description.get_text() for description in soup.find_all('p')]
+    # return the extracted data
+    return links, titles, descriptions
 
-# Function to extract titles from dawn.com
-def extract_dawn_titles():
-    url = "https://www.dawn.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    titles = [title.text for title in soup.find_all('h2')]
-    return titles
+# URL of the websites to scrape
+dawn_url = 'https://www.dawn.com/'
+bbc_url = 'https://www.bbc.com/'
 
-# Function to extract descriptions from dawn.com
-def extract_dawn_descriptions():
-    url = "https://www.dawn.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    descriptions = [desc.text for desc in soup.find_all('p')]
-    return descriptions
+# extract data from dawn.com
+dawn_links, dawn_titles, dawn_descriptions = extract_data(dawn_url)
 
-# Function to extract links from bbc.com
-def extract_bbc_links():
-    url = "https://www.bbc.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = [link.get('href') for link in soup.find_all('a')]
-    return links
+# extract data from bbc.com
+bbc_links, bbc_titles, bbc_descriptions = extract_data(bbc_url)
 
-# Function to extract titles from bbc.com
-def extract_bbc_titles():
-    url = "https://www.bbc.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    titles = [title.text for title in soup.find_all('h3')]
-    return titles
+# combine the extracted data
+extracted_data = {
+    'dawn': {'links': dawn_links, 'titles': dawn_titles, 'descriptions': dawn_descriptions},
+    'bbc': {'links': bbc_links, 'titles': bbc_titles, 'descriptions': bbc_descriptions}
+}
 
-# Function to extract descriptions from bbc.com
-def extract_bbc_descriptions():
-    url = "https://www.bbc.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    descriptions = [desc.text for desc in soup.find_all('p')]
-    return descriptions
-
-# Example usage
-dawn_links = extract_dawn_links()
-dawn_titles = extract_dawn_titles()
-dawn_descriptions = extract_dawn_descriptions()
-
-bbc_links = extract_bbc_links()
-bbc_titles = extract_bbc_titles()
-bbc_descriptions = extract_bbc_descriptions()
-
-print("Dawn Links:", dawn_links)
-print("Dawn Titles:", dawn_titles)
-print("Dawn Descriptions:", dawn_descriptions)
-
-print("BBC Links:", bbc_links)
-print("BBC Titles:", bbc_titles)
-print("BBC Descriptions:", bbc_descriptions)
+# save the extracted data to a JSON file
+with open('articles.json', 'w') as file:
+    json.dump(extracted_data, file)
